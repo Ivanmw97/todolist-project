964bd10e6d5f2fb679c7922788ef06b178efa8b2
Ivan <ivanmw97@gmail.com>
2025-01-27 13:13:22 +0000

    Configure deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     }
-  }
+  },
+  base: '/todolist-project/'
 })
